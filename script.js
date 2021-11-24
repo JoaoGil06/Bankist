ebfb48323f14c3febcc3cd0e7c1049ae54666bc1
JoaoGil06 <joaogmonteiro06@gmail.com>
2021-11-24 19:32:15 +0000

    Add displayMovements

--- script.js
+++ script.js
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-	owner: 'Jonas Schmedtmann',
+	owner: 'João Gil',
 	movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
 	interestRate: 1.2, // %
 	pin: 1111,
 };
 
 const account2 = {
-	owner: 'Jessica Davis',
+	owner: 'Rita Ferreira',
 	movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
 	interestRate: 1.5,
 	pin: 2222,
 };
 
 const account3 = {
-	owner: 'Steven Thomas Williams',
+	owner: 'Pedro Gil',
 	movements: [200, -200, 340, -300, -20, 50, 400, -460],
 	interestRate: 0.7,
 	pin: 3333,
 };
 
 const account4 = {
-	owner: 'Sarah Smith',
+	owner: 'Constantino Gil',
 	movements: [430, 1000, 700, 50, 90],
 	interestRate: 1,
 	pin: 4444,
@@ -61,16 +61,21 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = (movements) => {
+	containerMovements.innerHTML = '';
 
-const currencies = new Map([
-	['USD', 'United States dollar'],
-	['EUR', 'Euro'],
-	['GBP', 'Pound sterling'],
-]);
+	movements.forEach((movement, index) => {
+		const movementType = movement > 0 ? 'deposit' : 'withdrawal';
+		const html = `
+        <div class="movements__row">
+            <div class="movements__type movements__type--${movementType}">
+                ${index + 1} ${movementType}
+            </div>
+            <div class="movements__value">${movement}€</div>
+        </div>`;
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+		containerMovements.insertAdjacentHTML('afterbegin', html);
+	});
+};
 
-/////////////////////////////////////////////////
+displayMovements(account1.movements);
