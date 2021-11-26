10776775e38aeeb1bf2a13502191cfed192a3dff
ext.joao.g.monteiro <ext.joao.g.monteiro@farfetch.com>
2021-11-26 22:30:40 +0000

    Implement delete account

--- script.js
+++ script.js
@@ -177,3 +177,24 @@ btnTransfer.addEventListener("click", (event) => {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = "";
+});
